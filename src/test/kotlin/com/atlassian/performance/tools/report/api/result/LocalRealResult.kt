f6d4c38de9daa813c451cf032fb2841061632570
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2018-08-24 15:15:10 +0200

    Fix tests on Windows, which expected a filesystem-dependent test cohort names

--- src/test/kotlin/com/atlassian/performance/tools/report/api/result/LocalRealResult.kt
+++ src/test/kotlin/com/atlassian/performance/tools/report/api/result/LocalRealResult.kt
@@ -5,6 +5,7 @@ import com.atlassian.performance.tools.report.api.FullTimeline
 import com.atlassian.performance.tools.report.api.parser.MergingNodeCountParser
 import com.atlassian.performance.tools.report.api.parser.SystemMetricsParser
 import java.io.File
+import java.nio.file.FileSystem
 import java.nio.file.Path
 
 class LocalRealResult(
@@ -12,7 +13,7 @@ class LocalRealResult(
 ) {
 
     fun loadRaw(): CohortResult = FullCohortResult(
-        cohort = path.toString(),
+        cohort = path.joinToString(separator = "/") { it.toString() },
         results = File(
             this::class
                 .java
