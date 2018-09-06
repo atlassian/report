aadfa3fd45a0916b6605d59cb3c641b4667f7fcb
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2018-09-06 12:26:03 +0200

    JPERF-41 Add test for long action names in plain text report

--- src/test/kotlin/com/atlassian/performance/tools/report/api/PlaintextReportTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/report/api/PlaintextReportTest.kt
@@ -4,25 +4,25 @@ import com.atlassian.performance.tools.jiraactions.api.ActionMetric
 import com.atlassian.performance.tools.jiraactions.api.ActionMetricStatistics
 import com.atlassian.performance.tools.jiraactions.api.ActionResult.ERROR
 import com.atlassian.performance.tools.jiraactions.api.ActionResult.OK
-import com.atlassian.performance.tools.report.api.PlaintextReport
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import java.time.Duration
+import java.time.Duration.ofSeconds
 import java.time.Instant.now
 import java.util.UUID.randomUUID
 
 
 class PlaintextReportTest {
     private val actionMetrics = listOf(
-        ActionMetric("view", OK, Duration.ofSeconds(1), now(), randomUUID(), null),
-        ActionMetric("view", OK, Duration.ofSeconds(2), now(), randomUUID(), null),
-        ActionMetric("view", OK, Duration.ofSeconds(3), now(), randomUUID(), null),
-        ActionMetric("view", ERROR, Duration.ofSeconds(4), now(), randomUUID(), null),
-        ActionMetric("view", ERROR, Duration.ofSeconds(5), now(), randomUUID(), null),
-        ActionMetric("create", ERROR, Duration.ofSeconds(1), now(), randomUUID(), null),
-        ActionMetric("create", ERROR, Duration.ofSeconds(2), now(), randomUUID(), null),
-        ActionMetric("create", ERROR, Duration.ofSeconds(3), now(), randomUUID(), null),
-        ActionMetric("login", OK, Duration.ofSeconds(1), now(), randomUUID(), null)
+        ActionMetric("a very very very long action name", OK, ofSeconds(1), now(), randomUUID(), null),
+        ActionMetric("view", OK, ofSeconds(1), now(), randomUUID(), null),
+        ActionMetric("view", OK, ofSeconds(2), now(), randomUUID(), null),
+        ActionMetric("view", OK, ofSeconds(3), now(), randomUUID(), null),
+        ActionMetric("view", ERROR, ofSeconds(4), now(), randomUUID(), null),
+        ActionMetric("view", ERROR, ofSeconds(5), now(), randomUUID(), null),
+        ActionMetric("create", ERROR, ofSeconds(1), now(), randomUUID(), null),
+        ActionMetric("create", ERROR, ofSeconds(2), now(), randomUUID(), null),
+        ActionMetric("create", ERROR, ofSeconds(3), now(), randomUUID(), null),
+        ActionMetric("login", OK, ofSeconds(1), now(), randomUUID(), null)
     )
 
     @Test
@@ -33,6 +33,7 @@ class PlaintextReportTest {
 +---------------------------+---------------+----------+----------------------+
 | Action name               | sample size   | errors   | 95th percentile [ms] |
 +---------------------------+---------------+----------+----------------------+
+| a very very very long ... | 1             | 0        | 1000                 |
 | create                    | 0             | 3        | null                 |
 | login                     | 1             | 0        | 1000                 |
 | view                      | 3             | 2        | 3000                 |
