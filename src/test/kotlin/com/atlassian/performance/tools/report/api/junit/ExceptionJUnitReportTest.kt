2bc4f19744aed65032f451cf992eeb83314e6592
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2019-01-08 10:26:21 +0100

    JPERF-339 Increase test coverage in ExceptionJUnitReportTest

--- src/test/kotlin/com/atlassian/performance/tools/report/api/junit/ExceptionJUnitReportTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/report/api/junit/ExceptionJUnitReportTest.kt
@@ -2,25 +2,35 @@ package com.atlassian.performance.tools.report.api.junit
 
 import org.assertj.core.api.Assertions
 import org.junit.Test
+import java.io.PrintWriter
 
 class ExceptionJUnitReportTest {
 
     @Test
-    fun shouldStartWithXmlDeclaration() {
-        val report = ExceptionJUnitReport("TEST NAME", Exception("OOPS"))
+    fun shouldGenerateXmlReport() {
+        val report = ExceptionJUnitReport("TEST NAME", MockException("OOPS"))
+        val expectedXml = this::class.java.getResourceAsStream("./jUnitReport.xml").bufferedReader().use { it.readText() }
 
         val xml = report.toXml("TEST CLASS NAME")
 
-        Assertions.assertThat(xml).startsWith("""<?xml version="1.0" encoding="UTF-8"?>""")
+        Assertions.assertThat(xml).isEqualTo(expectedXml)
     }
 
     @Test
     fun shouldParsePercents() {
         val message = "OOPS%{"
-        val report = ExceptionJUnitReport("TEST NAME", Exception(message))
+        val report = ExceptionJUnitReport("TEST NAME", MockException(message))
 
         val xml = report.toXml("TEST CLASS NAME")
 
         Assertions.assertThat(xml).contains(message)
+    }
+
+    private class MockException(message: String) : Exception(message) {
+        override fun printStackTrace(printWriter: PrintWriter) {
+            printWriter.println("""java.lang.Exception: $message
+	at com.atlassian.performance.tools.report.api.junit.ExceptionJUnitReportTest.shouldGenerateXmlReport(ExceptionJUnitReportTest.kt:10)
+	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)""")
+        }
     }
 }
