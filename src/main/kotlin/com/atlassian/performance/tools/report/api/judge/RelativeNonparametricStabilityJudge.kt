0ec4887df51b42e0a1308585dc2477116e77ec2d
Stan Natkovskyi <snatkovskyi@atlassian.com>
2019-07-25 08:26:42 +0200

    JPERF-537: Add `failedActions` property to `Verdict`

--- src/main/kotlin/com/atlassian/performance/tools/report/api/judge/RelativeNonparametricStabilityJudge.kt
+++ src/main/kotlin/com/atlassian/performance/tools/report/api/judge/RelativeNonparametricStabilityJudge.kt
@@ -4,6 +4,7 @@ import com.atlassian.performance.tools.jiraactions.api.ActionType
 import com.atlassian.performance.tools.report.ActionMetricsReader
 import com.atlassian.performance.tools.report.api.ShiftedDistributionRegressionTest
 import com.atlassian.performance.tools.report.api.junit.FailedAssertionJUnitReport
+import com.atlassian.performance.tools.report.api.junit.JUnitReport
 import com.atlassian.performance.tools.report.api.junit.SuccessfulJUnitReport
 import com.atlassian.performance.tools.report.api.result.EdibleResult
 
@@ -18,25 +19,59 @@ class RelativeNonparametricStabilityJudge(
         baselineResult: EdibleResult,
         experimentResult: EdibleResult
     ): Verdict {
-        return Verdict(
-            actions.map { action ->
-                val label = action.label
-                val baselineCohort = baselineResult.cohort
-                val experimentCohort = experimentResult.cohort
-                val reportName = "Stability regression for $label $experimentCohort vs $baselineCohort"
-                val reader = ActionMetricsReader()
-                val baseline = reader.read(baselineResult.actionMetrics)[label]?.stats?.values
-                    ?: return@map FailedAssertionJUnitReport(reportName, "No action $label results for $baselineCohort")
-                val experiment = reader.read(experimentResult.actionMetrics)[label]?.stats?.values
-                    ?: return@map FailedAssertionJUnitReport(reportName, "No action $label results for $experimentCohort")
-                val test = ShiftedDistributionRegressionTest(baseline, experiment, mwAlpha = 0.0, ksAlpha = significance)
-                return@map if (!test.equalDistributionsAfterShift) {
-                    val message = "[$label] distribution shapes are different at $significance significance level"
-                    FailedAssertionJUnitReport(reportName, message)
-                } else {
-                    SuccessfulJUnitReport(testName = reportName)
-                }
+        val testReports = mutableListOf<JUnitReport>()
+        val failedActions = mutableListOf<ActionType<*>>()
+        actions.forEach { action ->
+            val actionReport = judge(
+                action,
+                baselineResult,
+                experimentResult
+            )
+            testReports.add(actionReport.report)
+            if (actionReport.nonExceptionalFailure) {
+                failedActions.add(actionReport.action)
             }
+        }
+
+        return Verdict(
+            reports = testReports,
+            failedActions = failedActions
         )
+    }
+
+    private fun judge(
+        action: ActionType<*>,
+        baselineResult: EdibleResult,
+        experimentResult: EdibleResult
+    ): ActionReport {
+        val label = action.label
+        val baselineCohort = baselineResult.cohort
+        val experimentCohort = experimentResult.cohort
+        val reportName = "Stability regression for $label $experimentCohort vs $baselineCohort"
+        val reader = ActionMetricsReader()
+        val baseline = reader.read(baselineResult.actionMetrics)[label]?.stats?.values
+            ?: return ActionReport(
+                report = FailedAssertionJUnitReport(reportName, "No action $label results for $baselineCohort"),
+                action = action
+            )
+        val experiment = reader.read(experimentResult.actionMetrics)[label]?.stats?.values
+            ?: return ActionReport(
+                report = FailedAssertionJUnitReport(reportName, "No action $label results for $experimentCohort"),
+                action = action
+            )
+        val test = ShiftedDistributionRegressionTest(baseline, experiment, mwAlpha = 0.0, ksAlpha = significance)
+        return if (!test.equalDistributionsAfterShift) {
+            val message = "[$label] distribution shapes are different at $significance significance level"
+            ActionReport(
+                report = FailedAssertionJUnitReport(reportName, message),
+                action = action,
+                nonExceptionalFailure = true
+            )
+        } else {
+            ActionReport(
+                report = SuccessfulJUnitReport(testName = reportName),
+                action = action
+            )
+        }
     }
 }
