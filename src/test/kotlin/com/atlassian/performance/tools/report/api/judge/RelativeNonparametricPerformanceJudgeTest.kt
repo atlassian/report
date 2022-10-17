9426bec3786461b654b8faa0df81eda1f164374f
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-10-17 16:45:15 +0200

    JPERF-832 Move integration test from JPT to this library

--- src/test/kotlin/com/atlassian/performance/tools/report/api/judge/RelativeNonparametricPerformanceJudgeTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/report/api/judge/RelativeNonparametricPerformanceJudgeTest.kt
@@ -1,6 +1,6 @@
 package com.atlassian.performance.tools.report.api.judge
 
-import com.atlassian.performance.tools.jiraactions.api.EDIT_ISSUE
+import com.atlassian.performance.tools.jiraactions.api.*
 import com.atlassian.performance.tools.report.api.ShiftedDistributionRegressionTest
 import com.atlassian.performance.tools.report.api.result.DurationData
 import com.atlassian.performance.tools.report.api.result.EdibleResult
@@ -9,6 +9,8 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
+import java.time.Duration
+import java.time.Instant
 import org.mockito.Mockito.`when` as whenever
 
 class RelativeNonparametricPerformanceJudgeTest {
@@ -50,5 +52,55 @@ class RelativeNonparametricPerformanceJudgeTest {
         assertThat(result.reports).hasSize(1)
         assertThat((result.reports[0] as FailedActionJunitReport).toXml(testClassName = "does not matter"))
             .contains("There is a regression in [Full Edit Issue] with 95% confidence level. Regression is larger than allowed +10.00% tolerance")
+    }
+
+    @Test
+    fun shouldVerdictHaveFailedReports() {
+        // given
+        fun generateBaseline(actionTypes: List<ActionType<*>>): List<ActionMetric> {
+            return LongRange(0, 120).map { index ->
+                actionTypes.map { actionType ->
+                    ActionMetric.Builder(
+                        actionType.label,
+                        ActionResult.OK,
+                        Duration.ofMillis(10).plusMillis(index * 10),
+                        Instant.now().minusSeconds(index)
+                    ).build()
+                }
+            }.flatten()
+        }
+
+        fun generateExperiment(actionTypes: List<ActionType<*>>): List<ActionMetric> {
+            return LongRange(0, 120).map { index ->
+                actionTypes.map { actionType ->
+                    ActionMetric.Builder(
+                        actionType.label,
+                        ActionResult.OK,
+                        Duration.ofSeconds(100),
+                        Instant.now().minusSeconds(index)
+                    ).build()
+                }
+            }.flatten()
+        }
+
+        val actionTypes: List<ActionType<*>> = listOf(EDIT_ISSUE, ADD_COMMENT)
+        val zeroToleranceRatios = actionTypes.associate { it to 0f }.toMap()
+        val baselineMetrics = generateBaseline(actionTypes)
+        val experimentMetrics = generateExperiment(actionTypes)
+
+        // when
+        val result = RelativeNonparametricPerformanceJudge.Builder().build()
+            .judge(
+                toleranceRatios = zeroToleranceRatios,
+                baselineResult = EdibleResult.Builder("baseline mock")
+                    .actionMetrics(baselineMetrics)
+                    .build(),
+                experimentResult = EdibleResult.Builder("experiment mock")
+                    .actionMetrics(experimentMetrics)
+                    .build()
+            )
+        // then
+        assertThat(result.reports).hasSize(2)
+        assertThat(result.reports).allSatisfy { !it.successful }
     }
 }
