8f4cc66e20b837589dd7f5de50ab46b304183bc7
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-10-17 09:50:59 +0200

    JPERF-832 Reword the log message again as it could look better in the logs

--- src/main/kotlin/com/atlassian/performance/tools/report/PercentageExtensions.kt
+++ src/main/kotlin/com/atlassian/performance/tools/report/PercentageExtensions.kt
@@ -1,4 +1,10 @@
 package com.atlassian.performance.tools.report
 
-fun Float.toPercentage(decimalPlaces: Int): String = "%+.${decimalPlaces}f%%".format(this * 100)
-fun Double.toPercentage(decimalPlaces: Int): String = "%+.${decimalPlaces}f%%".format(this * 100)
+fun Float.toPercentage(decimalPlaces: Int, includeSign: Boolean = true) = toDouble().toPercentage(decimalPlaces, includeSign)
+
+fun Double.toPercentage(decimalPlaces: Int, includeSign: Boolean = true): String {
+    return when {
+        includeSign -> "%+.${decimalPlaces}f%%".format(this * 100)
+        else -> "%.${decimalPlaces}f%%".format(this * 100)
+    }
+}
