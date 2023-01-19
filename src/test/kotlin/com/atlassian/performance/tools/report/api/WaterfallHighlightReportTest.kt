6ea1eff70f8feae3163e07e439f870a834823c1b
Przemyslaw Czuj <pczuj@atlassian.com>
2023-01-19 14:14:18 +0100

    JPERF-931: Add test that reproduces an issue with WaterfallHighlightReport

--- src/test/kotlin/com/atlassian/performance/tools/report/api/WaterfallHighlightReportTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/report/api/WaterfallHighlightReportTest.kt
@@ -4,6 +4,7 @@ import com.atlassian.performance.tools.jiraactions.api.parser.ActionMetricsParse
 import com.atlassian.performance.tools.workspace.api.RootWorkspace
 import org.junit.Test
 import java.nio.file.Paths
+import java.util.stream.Collectors
 
 class WaterfallHighlightReportTest {
 
@@ -11,6 +12,16 @@ class WaterfallHighlightReportTest {
     fun shouldReportMedian() {
         val metricsStream = javaClass.getResourceAsStream("/real-action-metrics-with-drilldown.jpt")
         val actionMetrics = ActionMetricsParser().parse(metricsStream)
+        val workspace = RootWorkspace(Paths.get("build/jpt-workspace")).currentTask.isolateTest("WaterfallHighlight")
+        val highlight = WaterfallHighlightReport()
+
+        highlight.report(actionMetrics, workspace)
+    }
+
+    @Test
+    fun `should not fail with real example of action metrics gathered on 2022 01 17`() {
+        val metricsStream = javaClass.getResourceAsStream("action-metrics-2022-01-17.jpt")!!
+        val actionMetrics = ActionMetricsParser().stream(metricsStream).collect(Collectors.toList())
         val workspace = RootWorkspace(Paths.get("build/jpt-workspace")).currentTask.isolateTest("WaterfallHighlight")
         val highlight = WaterfallHighlightReport()
 
