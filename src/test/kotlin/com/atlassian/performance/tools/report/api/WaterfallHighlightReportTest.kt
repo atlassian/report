5bf27e95fc2b4d90220d6c920d307eff82045c53
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-03-14 11:40:46 +0100

    JPERF-1021: Expect 2023 waterfalls for View Issue

--- src/test/kotlin/com/atlassian/performance/tools/report/api/WaterfallHighlightReportTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/report/api/WaterfallHighlightReportTest.kt
@@ -2,18 +2,36 @@ package com.atlassian.performance.tools.report.api
 
 import com.atlassian.performance.tools.jiraactions.api.parser.ActionMetricsParser
 import com.atlassian.performance.tools.workspace.api.RootWorkspace
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import java.nio.file.Paths
+import kotlin.streams.toList
 
 class WaterfallHighlightReportTest {
 
     @Test
     fun shouldReportMedian() {
-        val metricsStream = javaClass.getResourceAsStream("/real-action-metrics-with-drilldown.jpt")
-        val actionMetrics = ActionMetricsParser().parse(metricsStream)
-        val workspace = RootWorkspace(Paths.get("build/jpt-workspace")).currentTask.isolateTest("WaterfallHighlight")
+        report("/real-action-metrics-with-drilldown.jpt")
+    }
+
+    @Test
+    fun shouldHighlightIn2023() {
+        report("/real-action-metrics-with-drilldown-2023.jpt")
+    }
+
+    private fun report(resourceName: String) {
+        val metricsStream = javaClass.getResourceAsStream(resourceName)!!
+        val actionMetrics = ActionMetricsParser().stream(metricsStream).toList()
+        val workspace = RootWorkspace(Paths.get("build/jpt-workspace"))
+            .currentTask
+            .isolateTest("WaterfallHighlight/$resourceName")
         val highlight = WaterfallHighlightReport()
 
         highlight.report(actionMetrics, workspace)
+
+        val hasViewIssue = workspace.directory.toFile().walkTopDown().any { it.name == "View Issue" }
+        assertThat(hasViewIssue)
+            .`as`("$workspace has View Issue")
+            .isTrue()
     }
 }
