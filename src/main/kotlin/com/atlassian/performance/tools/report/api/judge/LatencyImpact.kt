f8755c556e76fcdf1611519072e2db170a3a3779
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-06-26 11:37:54 +0200

    JPERF-1160: Document `LatencyImpact` fields

--- src/main/kotlin/com/atlassian/performance/tools/report/api/judge/LatencyImpact.kt
+++ src/main/kotlin/com/atlassian/performance/tools/report/api/judge/LatencyImpact.kt
@@ -1,8 +1,15 @@
 package com.atlassian.performance.tools.report.api.judge
 
 import com.atlassian.performance.tools.jiraactions.api.ActionType
+import com.atlassian.performance.tools.jiraactions.api.VIEW_ISSUE
 import java.time.Duration
 
+/**
+ * @param action subject of the impact, e.g. this is how latency of [VIEW_ISSUE] was impacted
+ * @param relative relative size of the impact, e.g. 0.15 is +15% latency or -0.04 is -4% latency
+ * @param absolute absolute size of the impact, e.g +300 ms or -50 ms
+ * @param conclusive if true, then [relative] and [absolute] sizes should be considered a signal rather than noise
+ */
 class LatencyImpact private constructor(
     val action: ActionType<*>,
     val relative: Double,
