97ed5f3254007fc4597283f4d57a0455d6e6d539
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-06-28 15:47:12 +0200

    JPERF-1160: Merge nonparam judge test cases
    They all were testing a "regression case", just with assertions split
    across test methods. The `RelativeTypicalPerformanceJudgeTest` has test
    case split by buisness case: regression, improvement, noise.
    Let's do the same for the nonparam judge.
    
    TDD: refactor

--- src/test/kotlin/com/atlassian/performance/tools/report/api/judge/RelativeNonparametricPerformanceJudgeTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/report/api/judge/RelativeNonparametricPerformanceJudgeTest.kt
@@ -2,7 +2,6 @@ package com.atlassian.performance.tools.report.api.judge
 
 import com.atlassian.performance.tools.jiraactions.api.EDIT_ISSUE
 import com.atlassian.performance.tools.report.api.result.FakeResults
-import com.atlassian.performance.tools.report.junit.FailedActionJunitReport
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import java.time.Duration
@@ -10,35 +9,14 @@ import java.util.function.Consumer
 
 class RelativeNonparametricPerformanceJudgeTest {
 
-    private val zeroToleranceRatios = FakeResults.actionTypes.associate { it to 0.1f }.toMap()
-    private val tested = RelativeNonparametricPerformanceJudge.Builder().build()
-        .judge(
-            toleranceRatios = zeroToleranceRatios,
-            baselineResult = FakeResults.fastResult,
-            experimentResult = FakeResults.slowResult
-        )
-
     @Test
-    fun shouldVerdictHaveFailedReports() {
-        assertThat(tested.reports).hasSize(2)
-        assertThat(tested.reports).allSatisfy { !it.successful }
-    }
-
-    @Test
-    fun shouldFailedReportXmlHaveExpectedMessage() {
-        assertThat(tested.reports).hasSize(2)
-        tested.reports.first().let { report ->
-            assertThat(report).isInstanceOf(FailedActionJunitReport::class.java)
-            assertThat((report as FailedActionJunitReport).toXml(testClassName = "does not matter"))
-                .contains("There is a regression in [Full Edit Issue] with 95% confidence level. Regression is larger than allowed +10.00% tolerance")
-        }
-    }
-
-    @Test
-    fun shouldReportLatencyImpacts() {
+    fun shouldJudgeRegression() {
+        // given
+        val zeroToleranceRatios = FakeResults.actionTypes.associate { it to 0.1f }.toMap()
         val impacts = mutableListOf<LatencyImpact>()
 
-        RelativeNonparametricPerformanceJudge.Builder()
+        // when
+        val verdict = RelativeNonparametricPerformanceJudge.Builder()
             .handleLatencyImpact(Consumer { impacts.add(it) })
             .build()
             .judge(
@@ -47,6 +25,7 @@ class RelativeNonparametricPerformanceJudgeTest {
                 experimentResult = FakeResults.slowResult
             )
 
+        // then
         assertThat(impacts).isNotEmpty()
         assertThat(impacts.map { it.action }).contains(EDIT_ISSUE)
         assertThat(impacts.single { it.action == EDIT_ISSUE }).satisfies { editIssueImpact ->
@@ -54,6 +33,12 @@ class RelativeNonparametricPerformanceJudgeTest {
             assertThat(editIssueImpact.absolute).isBetween(Duration.ofMinutes(1), Duration.ofMinutes(2))
             assertThat(editIssueImpact.regression).isTrue()
             assertThat(editIssueImpact.signal).isTrue()
+        }
+        assertThat(verdict.reports).hasSize(2)
+        assertThat(verdict.reports).allSatisfy { !it.successful }
+        assertThat(verdict.reports.first()).satisfies { report ->
+            assertThat(report.toXml("dummyTestCase"))
+                .contains("There is a regression in [Full Edit Issue] with 95% confidence level. Regression is larger than allowed +10.00% tolerance")
         }
     }
 }
