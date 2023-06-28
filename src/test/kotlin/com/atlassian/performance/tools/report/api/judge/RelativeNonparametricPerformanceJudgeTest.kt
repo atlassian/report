cea36674649045418c1b81e66f43bf47bbf4e15f
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-06-28 17:09:06 +0200

    JPERF-1160: Test nonparam judge vs noise
    TDD: red

--- src/test/kotlin/com/atlassian/performance/tools/report/api/judge/RelativeNonparametricPerformanceJudgeTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/report/api/judge/RelativeNonparametricPerformanceJudgeTest.kt
@@ -6,10 +6,41 @@ import com.atlassian.performance.tools.report.api.result.FakeResults
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.SoftAssertions
 import org.junit.Test
+import java.time.Duration.ofMillis
 import java.time.Duration.ofMinutes
 import java.util.function.Consumer
 
 class RelativeNonparametricPerformanceJudgeTest {
+
+    @Test
+    fun shouldJudgeNoise() {
+        // given
+        val zeroToleranceRatios = FakeResults.actionTypes.associate { it to 0.1f }.toMap()
+        val impacts = mutableListOf<LatencyImpact>()
+        val judge = RelativeNonparametricPerformanceJudge.Builder()
+            .handleLatencyImpact(Consumer { impacts.add(it) })
+            .build()
+
+        // when
+        val verdict = judge.judge(
+            toleranceRatios = zeroToleranceRatios,
+            baselineResult = FakeResults.fastResult,
+            experimentResult = FakeResults.fastResult
+        )
+
+        // then
+        assertThat(verdict.reports).hasSize(2)
+        assertThat(verdict.reports).allMatch { it.successful }
+        assertThat(impacts).isNotEmpty()
+        assertThat(impacts.map { it.action }).contains(EDIT_ISSUE)
+        assertThat(impacts.single { it.action == EDIT_ISSUE }).satisfies { editIssueImpact ->
+            assertThat(editIssueImpact.relative).isBetween(-0.1, 0.1)
+            assertThat(editIssueImpact.absolute).isBetween(ofMillis(-1), ofMillis(1))
+            assertThat(editIssueImpact.regression).isFalse()
+            assertThat(editIssueImpact.improvement).isFalse()
+            assertThat(editIssueImpact.noise).isTrue()
+        }
+    }
 
     @Test
     fun shouldJudgeRegression() {
