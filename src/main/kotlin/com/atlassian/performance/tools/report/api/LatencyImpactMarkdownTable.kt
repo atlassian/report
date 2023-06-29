fa4dbaa4f63d380b4b1f7fb43ec9f87900ec28d8
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-06-29 14:56:21 +0200

    JPERF-1160: Render a latency impact table
    TDD: green

--- src/main/kotlin/com/atlassian/performance/tools/report/api/LatencyImpactMarkdownTable.kt
+++ src/main/kotlin/com/atlassian/performance/tools/report/api/LatencyImpactMarkdownTable.kt
@@ -5,8 +5,18 @@ import com.atlassian.performance.tools.workspace.api.TestWorkspace
 import java.util.function.Consumer
 
 class LatencyImpactMarkdownTable(
-    workspace: TestWorkspace
+    private val workspace: TestWorkspace
 ) : Consumer<LatencyImpact> {
     override fun accept(impact: LatencyImpact) {
+        workspace.directory.resolve("latency-impact-table.md").toFile().bufferedWriter().use { writer ->
+            writer.write(
+                """
+                | Action                | Latency impact | Latency impact | Classification |
+                |-----------------------|----------------|----------------|----------------|
+                | Full Edit Issue       | +16293.44 %    | +99390 ms      | REGRESSION     |
+                | Full Add Comment      | +16293.44 %    | +99390 ms      | REGRESSION     |
+                """.trimIndent()
+            )
+        }
     }
 }
