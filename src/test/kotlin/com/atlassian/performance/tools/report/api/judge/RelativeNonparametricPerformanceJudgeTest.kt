eae0b056a9863bdf06314defc678c681c1d0d211
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-06-29 11:58:27 +0200

    JPERF-1160: Assert on impacts softly
    When the "improvement" case fails, it was failing on:
    ```
    assertThat(editIssueImpact.improvement).isTrue()
    assertThat(editIssueImpact.signal).isTrue()
    ```
    
    The improvement assertion was red, so we didn't reach the signal
    assertion. It means that we don't know if the judge thinks it's a
    regression or noise. So we use soft assertions to see both checks at
    once.
    
    Using soft assertions has two consequences:
    * we don't see the actual assertion line in stacktrace, so we don't know
      which boolean `isTrue` is failing, so we need the `as` string
    * it doesn't workt with `satisfies`: https://github.com/assertj/assertj/issues/1353
      so we just use a local var
    
    Before:
    ```
    org.junit.ComparisonFailure: expected:<[tru]e> but was:<[fals]e>
    ...
    	at com.atlassian.performance.tools.report.api.judge.RelativeNonparametricPerformanceJudgeTest$shouldJudgeImprovement$4.accept(RelativeNonparametricPerformanceJudgeTest.kt:68)
    	at com.atlassian.performance.tools.report.api.judge.RelativeNonparametricPerformanceJudgeTest$shouldJudgeImprovement$4.accept(RelativeNonparametricPerformanceJudgeTest.kt:11)
    	at com.atlassian.performance.tools.report.api.judge.RelativeNonparametricPerformanceJudgeTest.shouldJudgeImprovement(RelativeNonparametricPerformanceJudgeTest.kt:65)
    ```
    
    After:
    ```
    org.assertj.core.api.SoftAssertionError:
    The following 2 assertions failed:
    1) [improvement] expected:<[tru]e> but was:<[fals]e>
    at RelativeNonparametricPerformanceJudgeTest.shouldJudgeImprovement(RelativeNonparametricPerformanceJudgeTest.java:72) expected:<[tru]e> but was:<[fals]e>
    2) [signal] expected:<[tru]e> but was:<[fals]e>
    at RelativeNonparametricPerformanceJudgeTest.shouldJudgeImprovement(RelativeNonparametricPerformanceJudgeTest.java:73) expected:<[tru]e> but was:<[fals]e>
    
    	at org.assertj.core.api.SoftAssertions.assertAll(SoftAssertions.java:155)
    	at com.atlassian.performance.tools.report.api.judge.RelativeNonparametricPerformanceJudgeTest.shouldJudgeImprovement(RelativeNonparametricPerformanceJudgeTest.kt:74)
    ```
    
    TDD: red

--- src/test/kotlin/com/atlassian/performance/tools/report/api/judge/RelativeNonparametricPerformanceJudgeTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/report/api/judge/RelativeNonparametricPerformanceJudgeTest.kt
@@ -4,6 +4,7 @@ import com.atlassian.performance.tools.jiraactions.api.EDIT_ISSUE
 import com.atlassian.performance.tools.report.api.junit.JUnitReport
 import com.atlassian.performance.tools.report.api.result.FakeResults
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.SoftAssertions
 import org.junit.Test
 import java.time.Duration.ofMinutes
 import java.util.function.Consumer
@@ -28,16 +29,18 @@ class RelativeNonparametricPerformanceJudgeTest {
 
         // then
         assertThat(verdict.reports).hasSize(2)
-        assertThat(verdict.reports).allMatch { !it.successful }
-        assertThat(verdict.reports.first().extractText())
-            .contains("There is a regression in [Full Edit Issue] with 95% confidence level. Regression is larger than allowed +10.00% tolerance")
-        assertThat(impacts).isNotEmpty()
-        assertThat(impacts.map { it.action }).contains(EDIT_ISSUE)
-        assertThat(impacts.single { it.action == EDIT_ISSUE }).satisfies { editIssueImpact ->
-            assertThat(editIssueImpact.relative).isBetween(160.0, 165.0)
-            assertThat(editIssueImpact.absolute).isBetween(ofMinutes(1), ofMinutes(2))
-            assertThat(editIssueImpact.regression).isTrue()
-            assertThat(editIssueImpact.signal).isTrue()
+        with(SoftAssertions()) {
+            assertThat(verdict.reports).allMatch { !it.successful }
+            assertThat(verdict.reports.first().extractText())
+                .contains("There is a regression in [Full Edit Issue] with 95% confidence level. Regression is larger than allowed +10.00% tolerance")
+            assertThat(impacts).isNotEmpty()
+            assertThat(impacts.map { it.action }).contains(EDIT_ISSUE)
+            val impact = impacts.single { it.action == EDIT_ISSUE }
+            assertThat(impact.relative).isBetween(160.0, 165.0)
+            assertThat(impact.absolute).isBetween(ofMinutes(1), ofMinutes(2))
+            assertThat(impact.regression).`as`("regression").isTrue()
+            assertThat(impact.signal).`as`("signal").isTrue()
+            assertAll()
         }
     }
 
@@ -59,14 +62,16 @@ class RelativeNonparametricPerformanceJudgeTest {
 
         // then
         assertThat(verdict.reports).hasSize(2)
-        assertThat(verdict.reports).allMatch { it.successful }
         assertThat(impacts).isNotEmpty()
-        assertThat(impacts.map { it.action }).contains(EDIT_ISSUE)
-        assertThat(impacts.single { it.action == EDIT_ISSUE }).satisfies { editIssueImpact ->
-            assertThat(editIssueImpact.relative).isBetween(-0.994, -0.993)
-            assertThat(editIssueImpact.absolute).isBetween(ofMinutes(-2), ofMinutes(-1))
-            assertThat(editIssueImpact.improvement).isTrue()
-            assertThat(editIssueImpact.signal).isTrue()
+        with(SoftAssertions()) {
+            assertThat(verdict.reports).allMatch { it.successful }
+            assertThat(impacts.map { it.action }).contains(EDIT_ISSUE)
+            val impact = impacts.single { it.action == EDIT_ISSUE }
+            assertThat(impact.relative).isBetween(-0.994, -0.993)
+            assertThat(impact.absolute).isBetween(ofMinutes(-2), ofMinutes(-1))
+            assertThat(impact.improvement).`as`("improvement").isTrue()
+            assertThat(impact.signal).`as`("signal").isTrue()
+            assertAll()
         }
     }
 
