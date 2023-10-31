61b1dc56f26e7bf6758873128696fd40a719cb1c
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-10-31 22:24:37 +0100

    JPERF-1409 Apply minor changes from discussion in PR
    https://github.com/atlassian/report/pull/99

--- src/main/kotlin/com/atlassian/performance/tools/report/jfr/CountingOutputStream.kt
+++ src/main/kotlin/com/atlassian/performance/tools/report/jfr/CountingOutputStream.kt
@@ -4,33 +4,34 @@ import java.io.OutputStream
 
 /**
  * We're not using DataOutputStream because [java.io.DataOutputStream.size] is int which will overflow with our data size.
+ * Moreover, we're keeping two counters. One for absolute position, second for resettable offset.
  */
 class CountingOutputStream(private val decorated: OutputStream) : OutputStream() {
-    var count = 0L
+    var position = 0L
         private set
-    var countSinceLastReset = 0L
+    var offsetSinceLastReset = 0L
         private set
 
     override fun write(b: Int) {
         decorated.write(b)
-        count++
-        countSinceLastReset++
+        position++
+        offsetSinceLastReset++
     }
 
     override fun write(b: ByteArray) {
         decorated.write(b)
-        count += b.size
-        countSinceLastReset += b.size
+        position += b.size
+        offsetSinceLastReset += b.size
     }
 
     override fun write(b: ByteArray, offset: Int, length: Int) {
         decorated.write(b, offset, length)
-        count += length
-        countSinceLastReset += length
+        position += length
+        offsetSinceLastReset += length
     }
 
-    fun resetCount() {
-        countSinceLastReset = 0L
+    fun resetOffset() {
+        offsetSinceLastReset = 0L
     }
 
     override fun flush() {
