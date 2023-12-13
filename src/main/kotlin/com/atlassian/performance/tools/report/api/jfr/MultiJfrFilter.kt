28ef2c47b357d3582edafa646be9e561c5794130
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-12-13 12:03:47 +0100

    JPERF-1409 Add MultiJfrFilter

---
+++ src/main/kotlin/com/atlassian/performance/tools/report/api/jfr/MultiJfrFilter.kt
@@ -0,0 +1,94 @@
+package com.atlassian.performance.tools.report.api.jfr
+
+import com.atlassian.performance.tools.report.jfr.FilteringJfrWriter
+import jdk.jfr.consumer.RecordedEvent
+import org.openjdk.jmc.flightrecorder.testutils.parser.*
+import java.io.Closeable
+import java.nio.file.Path
+import java.util.function.Predicate
+
+/**
+ * Creates multiple JFR files from a single JFR file.
+ * MultiJfrFilter.filter() with n outputs is faster than n x JfrFilter.filter()
+ */
+class MultiJfrFilter private constructor(
+    private val input: Path,
+    private val outputs: List<FilteredOutput>
+) {
+
+    private class CompositeJfrEventListener(
+        private val listeners: List<ChunkParserListener>
+    ) : ChunkParserListener {
+
+        override fun onChunkStart(chunkIndex: Int, header: ChunkHeader) {
+            listeners.forEach { it.onChunkStart(chunkIndex, header) }
+        }
+
+        override fun onChunkEnd(chunkIndex: Int, skipped: Boolean) {
+            listeners.forEach { it.onChunkEnd(chunkIndex, skipped) }
+        }
+
+        override fun onMetadata(
+            eventHeader: EventHeader,
+            metadataPayload: ByteArray,
+            metadata: MetadataEvent
+        ) {
+            listeners.forEach { it.onMetadata(eventHeader, metadataPayload, metadata) }
+        }
+
+        override fun onEvent(event: RecordedEvent, eventHeader: EventHeader, eventPayload: ByteArray) {
+            listeners.forEach { it.onEvent(event, eventHeader, eventPayload) }
+        }
+
+        override fun onCheckpoint(eventHeader: EventHeader, eventPayload: ByteArray) {
+            listeners.forEach { it.onCheckpoint(eventHeader, eventPayload) }
+        }
+
+        override fun onRecordingStart() {
+            listeners.forEach { it.onRecordingStart() }
+        }
+
+        override fun onRecordingEnd() {
+            listeners.forEach { it.onRecordingEnd() }
+        }
+
+    }
+
+    fun filter() {
+        val streams = mutableListOf<Closeable>()
+        try {
+            val writers = outputs.map {
+                val stream = it.path.toFile().outputStream().buffered()
+                streams.add(stream)
+                FilteringJfrWriter(it.path.toFile(), stream, it.filter)
+            }
+            val compositeListener = CompositeJfrEventListener(writers)
+            val parser = StreamingChunkParser(compositeListener)
+            parser.parse(input)
+        } finally {
+            streams.forEach { it.close() }
+        }
+    }
+
+    private class FilteredOutput(
+        val path: Path,
+        val filter: Predicate<RecordedEvent>
+    )
+
+    class Builder(
+        private val input: Path
+    ) {
+        private val outputs = mutableListOf<FilteredOutput>()
+
+        fun output(output: Path, filter: Predicate<RecordedEvent>): Builder {
+            outputs.add(FilteredOutput(output, filter))
+            return this
+        }
+
+        fun build(): MultiJfrFilter {
+            check(outputs.isNotEmpty()) { "At least one output must be specified" }
+            return MultiJfrFilter(input, outputs)
+        }
+    }
+
+}
---
+++ src/test/kotlin/com/atlassian/performance/tools/report/jfr/MultiJfrFilterTest.kt
@@ -0,0 +1,60 @@
+package com.atlassian.performance.tools.report.jfr
+
+import com.atlassian.performance.tools.report.api.jfr.JfrFilter
+import com.atlassian.performance.tools.report.api.jfr.MultiJfrFilter
+import com.atlassian.performance.tools.report.api.result.CompressedResult
+import jdk.jfr.consumer.RecordedEvent
+import org.apache.logging.log4j.LogManager
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import java.io.File
+import java.nio.file.Path
+import java.util.function.Function
+import java.util.function.Predicate
+import kotlin.system.measureTimeMillis
+
+class MultiJfrFilterTest {
+    private val logger = LogManager.getLogger(this::class.java)
+
+    @Test
+    fun shouldBeFasterThanSingle() {
+        // given
+        val zippedInput = File(javaClass.getResource("/profiler-result.zip")!!.toURI())
+        val sampleInput = CompressedResult.unzip(zippedInput).resolve("profiler-result.jfr")
+
+        val multiFilterBuilder = MultiJfrFilter.Builder(sampleInput)
+        val singleJfrFilters = mutableListOf<JfrFilter>()
+        repeat(10) { index ->
+            val output =
+                sampleInput.resolveSibling("shouldBeFasterThanSingle-$index-" + sampleInput.fileName.toString())
+            val filter = Predicate<RecordedEvent> {
+                val javaThreadId = it.javaThreadId()
+                javaThreadId == 670L
+            }
+            multiFilterBuilder.output(output, filter)
+            singleJfrFilters.add(
+                JfrFilter.Builder()
+                    .eventFilter(filter)
+                    .filteredRecording(Function { _: Path -> output })
+                    .build()
+            )
+        }
+        val multiFilter = multiFilterBuilder.build()
+        // when
+        logger.debug("Filtering JFR using MultiJfrFilter implementation ...")
+        val multiDuration = measureTimeMillis {
+            multiFilter.filter()
+        }
+        logger.debug("MultiJfrFilter duration: $multiDuration ms")
+
+        logger.debug("Filtering JFR using JfrFilter implementation ...")
+        val singleDuration = measureTimeMillis {
+            singleJfrFilters.forEach { it.filter(sampleInput) }
+        }
+        logger.debug("SingleJfrFilters duration: $singleDuration ms")
+        // then
+        assertThat(multiDuration)
+            .describedAs("Multi duration should be at least 60% faster than single - given enough outputs to produce")
+            .isLessThan((singleDuration * 0.4).toLong())
+    }
+}
