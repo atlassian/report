8d3b0da81f9bf6961fd6ef3ec3e78afae1e690b7
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2024-01-11 18:09:16 +0100

    JPERF-1454: Refactor into a linear "train"
    TDD: green

--- src/test/kotlin/com/atlassian/performance/tools/report/chart/RainbowTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/report/chart/RainbowTest.kt
@@ -2,6 +2,8 @@ package com.atlassian.performance.tools.report.chart
 
 import com.atlassian.performance.tools.jiraactions.api.ActionMetric
 import com.atlassian.performance.tools.jiraactions.api.parser.ActionMetricsParser
+import com.atlassian.performance.tools.jiraactions.api.w3c.PerformanceNavigationTiming
+import com.atlassian.performance.tools.jiraactions.api.w3c.PerformanceResourceTiming
 import com.atlassian.performance.tools.report.chart.waterfall.WaterfallChart
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.SoftAssertions
@@ -10,6 +12,7 @@ import java.io.File
 import java.time.Duration
 import java.time.Duration.ZERO
 import java.time.Duration.ofMillis
+import java.time.temporal.ChronoUnit.MILLIS
 import kotlin.streams.toList
 
 class RainbowTest {
@@ -43,7 +46,7 @@ class RainbowTest {
                 it.assertThat(processing).isEqualTo(ofMillis(72))
                 it.assertThat(load).isEqualTo(ofMillis(1))
                 it.assertThat(excessResource).isEqualTo(ofMillis(184))
-                it.assertThat(excessJavascript).isEqualTo(ofMillis(184))
+                it.assertThat(excessJavascript.truncatedTo(MILLIS)).isEqualTo(ofMillis(53))
                 it.assertThat(total).isEqualTo(ofMillis(546).plusNanos(821000))
                 it.assertThat(unexplained).isLessThan(ofMillis(100))
             }
@@ -59,24 +62,67 @@ class RainbowTest {
 
     private fun inferRainbow(metric: ActionMetric): Rainbow {
         val nav = metric.drilldown!!.navigations.single()
-        val resources = metric.drilldown!!.resources
-        val lastResource = resources.map { it.responseEnd }.max() ?: nav.loadEventEnd
-        val excessResource = lastResource - nav.loadEventEnd
         return with(nav.resource) {
+            val train = TimeTrain(redirectStart)
+            val redirect = train.jumpOff(redirectEnd)
+            val preWorker = train.jumpOff(workerStart)
+            val serviceWorkerInit = train.jumpOff(fetchStart)
+            val fetchAndCache = train.jumpOff(domainLookupStart)
+            val dns = train.jumpOff(domainLookupEnd)
+            val preConnect = train.jumpOff(connectStart)
+            val tcp = train.jumpOff(connectEnd)
+            val preRequest = train.jumpOff(requestStart)
+            val request = train.jumpOff(responseStart)
+            val response = train.jumpOff(responseEnd)
+            val processing = train.jumpOff(nav.domComplete)
+            val preLoad = train.jumpOff(nav.loadEventStart)
+            val load = train.jumpOff(nav.loadEventEnd)
+            val lastResource = metric.drilldown!!.resources.map { it.responseEnd }.max()
+            val excessResource = train.jumpOff(lastResource ?: ZERO)
+            val excessJavascript = train.jumpOff(metric.duration)
             Rainbow(
-                redirect = redirectEnd - redirectStart,
-                serviceWorkerInit = if (workerStart != ZERO) fetchStart - workerStart else ZERO,
-                fetchAndCache = domainLookupStart - fetchStart,
-                dns = domainLookupEnd - domainLookupEnd,
-                tcp = connectEnd - connectStart,
-                request = responseStart - requestStart,
-                response = responseEnd - responseStart,
-                processing = if (nav.domComplete != ZERO) nav.domComplete - responseEnd else ZERO,
-                load = nav.loadEventEnd - nav.loadEventStart,
+                redirect = redirect,
+                serviceWorkerInit = serviceWorkerInit,
+                fetchAndCache = fetchAndCache,
+                dns = dns,
+                tcp = tcp,
+                request = request,
+                response = response,
+                processing = processing,
+                load = load,
                 excessResource = excessResource,
-                excessJavascript = metric.duration - lastResource,
+                excessJavascript = excessJavascript,
                 total = metric.duration
             )
+        }
+    }
+
+    class TimeTrain(
+        private var lastStation: Duration
+    ) {
+        /**
+         * Jump off at the next stop
+         *
+         * @return how much time elapsed since the last stop, a linear time segment
+         */
+        fun jumpOff(nextStation: Duration): Duration {
+            /**
+             * Some stations are optional, e.g. [PerformanceResourceTiming.workerStart]
+             * or might not have happened yet, e.g. before [PerformanceNavigationTiming.loadEventStart]
+             */
+            if (nextStation == ZERO) {
+                return ZERO
+            }
+            /**
+             * Some stations are parallel and can come in different order in runtime,
+             * e.g. [PerformanceNavigationTiming.unloadEventEnd] might come before or after [PerformanceResourceTiming.redirectEnd].
+             */
+            if (nextStation < lastStation) {
+                return ZERO
+            }
+            val segment = nextStation - lastStation
+            lastStation = nextStation
+            return segment
         }
     }
 
