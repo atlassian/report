4f8b734ff162f00d5fe58b64137b581e9808999e
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2024-01-18 11:59:50 +0100

    JPERF-1454 Move rainbow to API, with a better name
    TDD: refactor

---
+++ src/main/kotlin/com/atlassian/performance/tools/report/api/drilldown/ActionMetricExplainer.kt
@@ -0,0 +1,39 @@
+package com.atlassian.performance.tools.report.api.drilldown
+
+import com.atlassian.performance.tools.jiraactions.api.ActionMetric
+import com.atlassian.performance.tools.report.drilldown.TimeTrain
+import java.time.Instant
+
+object ActionMetricExplainer {
+    fun explainDuration(metric: ActionMetric): DurationDrilldown {
+        val drilldown = metric.drilldown!!
+        val nav = drilldown.navigations.single()
+        val timeOrigin = drilldown.timeOrigin!!
+        return with(nav.resource) {
+            val train = TimeTrain(metric.start, metric.end, timeOrigin)
+            val lastResource = drilldown.resources
+                .map { timeOrigin + it.responseEnd }
+                .filter { it < metric.end }
+                .max() ?: Instant.MIN
+
+            DurationDrilldown.Builder(metric.duration)
+                .preNav(train.jumpOff(redirectStart))
+                .redirect(train.jumpOff(redirectEnd))
+                .preWorker(train.jumpOff(workerStart))
+                .serviceWorkerInit(train.jumpOff(fetchStart))
+                .fetchAndCache(train.jumpOff(domainLookupStart))
+                .dns(train.jumpOff(domainLookupEnd))
+                .preConnect(train.jumpOff(connectStart))
+                .tcp(train.jumpOff(connectEnd))
+                .preRequest(train.jumpOff(requestStart))
+                .request(train.jumpOff(responseStart))
+                .response(train.jumpOff(responseEnd))
+                .domProcessing(train.jumpOff(nav.domComplete))
+                .preLoad(train.jumpOff(nav.loadEventStart))
+                .load(train.jumpOff(nav.loadEventEnd))
+                .lateResources(train.jumpOff(lastResource))
+                .excessProcessing(train.jumpOff(metric.end))
+                .build()
+        }
+    }
+}
